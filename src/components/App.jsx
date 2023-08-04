2a9273ef8a206e965a117ccfbf32c00103608b3e
Pashttett <pavlo3637@gmail.com>
2023-08-04 14:38:55 +0300

    goit-react-hw-02-phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
+import React, { useState } from 'react';
+import ContactForm from './AddContactForm/AddContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const handleAddContact = (name, number) => {
+    const isContactExists = contacts.some(
+      (contact) => contact.name.toLowerCase() === name.toLowerCase() || contact.number === number
+    );
+
+    if (isContactExists) {
+      alert(`${name} or phone number ${number} is already in the phonebook`);
+      return;
+    }
+
+    const newContact = {
+      id: `id-${Date.now()}`,
+      name,
+      number,
+    };
+
+    setContacts((prevContacts) => [...prevContacts, newContact]);
+  };
+
+  const handleDeleteContact = (id) => {
+    setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== id));
+  };
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onAddContact={handleAddContact} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={handleFilterChange} />
+      <ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact} />
     </div>
   );
 };
+
+export default App;
---
+++ src/components/ContactItem/ContactItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types'; 
+
+const ContactItem = ({ contact, onDeleteContact }) => {
+
+  return (
+    <ListItem>
+      <ContactName>{contact.name}</ContactName>
+      <DeleteButton onClick={handleDelete}>Delete</DeleteButton>
+    </ListItem>
+  );
+};
+
+export default ContactItem;
+
+ContactItem.propTypes = {
+  contact: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+  }).isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+};
