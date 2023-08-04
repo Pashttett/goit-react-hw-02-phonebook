461aae88a40c76b260d66318b785bc5c96408a78
Pashttett <pavlo3637@gmail.com>
2023-08-04 16:03:39 +0300

    goit-react-hw-02-phonebook

--- src/components/ContactList/ContactList.styled.js
+++ src/components/ContactList/ContactList.styled.js
@@ -2,11 +2,11 @@ import styled from 'styled-components';
 
 export const ContactListContainer = styled.ul`
   list-style: none;
-  padding: 0;
+  padding: 10; 
 `;
 
 export const ContactListItem = styled.li`
-  margin-bottom: 15px;
+  margin-bottom: 20px;
   &:last-child {
     margin-bottom: 0;
   }
@@ -16,7 +16,7 @@ export const DeleteButton = styled.button`
   background-color: #800000;
   color: #fff;
   border: none;
-  border-radius: 4px;
+  border-radius: 14px;
   cursor: pointer;
   padding: 5px 10px;
   transition: background-color 0.3s;
