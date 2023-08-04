a34331c87019719a87cead866b4188ac0210d4e2
Pashttett <pavlo3637@gmail.com>
2023-08-04 15:10:27 +0300

    Revert "goit-react-hw-02-phonebook"
    This reverts commit ec3be181dfc4a5dc361ef476e056795c2909fc36.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,52 @@
-import AddContactForm from './AddContactForm/AddContactForm';
+import React, { useState } from 'react';
+import ContactForm from './AddContactForm/AddContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  handleAddContact = (name, number) => {
+  const handleAddContact = (name, number) => {
+    const isContactExists = contacts.some(
+      (contact) => contact.name.toLowerCase() === name.toLowerCase() || contact.number === number
+    );
+
+    if (isContactExists) {
+      alert(`${name} or phone number ${number} is already in the phonebook`);
+      return;
+    }
+
     const newContact = {
       id: `id-${Date.now()}`,
       name,
       number,
     };
 
-    this.setState((prevState) => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+    setContacts((prevContacts) => [...prevContacts, newContact]);
   };
 
-  handleFilterChange = (e) => {
-    this.setState({ filter: e.target.value });
+  const handleDeleteContact = (id) => {
+    setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== id));
   };
 
-  handleDeleteContact = (id) => {
-    this.setState((prevState) => ({
-      contacts: prevState.contacts.filter((contact) => contact.id !== id),
-    }));
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
   };
 
-  render() {
-    const { contacts, filter } = this.state;
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
-    const filteredContacts = contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <AddContactForm onAddContact={this.handleAddContact} />
-        <h2>Contacts</h2>
-        <Filter value={filter} onChange={this.handleFilterChange} />
-        <ContactList contacts={filteredContacts} onDeleteContact={this.handleDeleteContact} />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onAddContact={handleAddContact} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={handleFilterChange} />
+      <ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact} />
+    </div>
+  );
+};
 
 export default App;
